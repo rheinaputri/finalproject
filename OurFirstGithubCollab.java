21dc02bade706e13ca78a46297b6d658f7abad25
raysyid <ridhxrasyid@gmail.com>
2023-09-13 14:36:40 +0700

    modifikasi Rasyid

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -4,5 +4,6 @@ public class OurFirstGithubCollab {
         System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
 
         System.out.println("Ketua mulai mengembangkan fitur kedua");
+        System.out.println("Tambahan dari Rasyid");
     } 
 }
