039fec04e5025392f9bc491fa48bb88cf52781a3
rheinaputri <rheinaputri8@gmail.com>
2023-09-13 14:14:22 +0700

    mulai fitur kedua

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -1,8 +1,8 @@
 public class OurFirstGithubCollab {
     public static void main(String[] args){
 
-        System.out.println("Ini ceritanya fitur pertama sudah berjalan dbaik");
+        System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
 
-        System.out.println("rheina mulai mengembangkan fitur kedua");
+        System.out.println("ketua mulai mengembangkan fitur kedua");
     } 
 }
