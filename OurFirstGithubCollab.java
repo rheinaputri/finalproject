52650fe7d59c7012743b5aa9ecf2f30562207305
rheinaputri <rheinaputri8@gmail.com>
2023-09-13 14:48:36 +0700

    update main

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -1,5 +1,5 @@
 public class OurFirstGithubCollab {
     public static void main(String[] args){
-        System.out.println("Ini ceritanya fitur pertama sudah berjalan dbaik")
+        System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
     } 
 }
