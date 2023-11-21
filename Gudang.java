97272a7de1cbb8c7bcd3d3fd4138b8444e9ab6ee
raysyid <ridhxrasyid@gmail.com>
2023-11-21 08:29:04 +0700

    barang baru & barang sudah ada

--- Gudang.java
+++ Gudang.java
@@ -4,7 +4,7 @@ public class Gudang {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int stok, barangmasuk, barangkeluar, lakukan, total;
+        int stok, barangmasuk, barangkeluar, lakukan, total, opsi;
         String login, username, password, namabarang, kode, next;
 
         String[][] barang = new String[7][4];
@@ -64,45 +64,87 @@ public class Gudang {
             }
             System.out.print("Lakukan : ");
             lakukan = sc.nextInt();
-//barangmasuk
+//dataBarang
             if (lakukan == 1) {
-                //Menampilkan nilai array
                 System.out.printf("%-15s %-15s %-15s %-15s\n", barang[0][0], barang[0][1], barang[0][2], barang[0][3]);
 
                 for (int i = 1; i < barang.length; i++) {
                     System.out.printf("%-15s %-15s %-15s %s\n", barang[i][0], barang[i][1], barang[i][2], jumlah[i][0]);
                 }
                 System.out.println("=================================================");
-
+// barangMasuk
             } else if (lakukan == 2) {
-                //Mengisi nilai array
-                int i = 1;
-                while (true) {
-                    System.out.print("Kode Barang : ");
-                    String kodeBaru = sc.next();
-                    barang[4+i][0] = kodeBaru;
-                    System.out.print("Nama Barang : ");
-                    String namaBaru = sc.next();
-                    sc.nextLine();
-                    barang[4+i][1] = namaBaru;
-                    System.out.print("Lokasi : ");
-                    String lokasiBaru = sc.nextLine();
-                    barang[4+i][2] = lokasiBaru;
-                    System.out.print("Jumlah : ");
-                    int jumlahBaru = sc.nextInt();
-                    jumlah[4+i][0] = jumlahBaru;
-                    sc.nextLine();
+                System.out.println("1. Barang baru");
+                System.out.println("2. Barang yang sudah ada");
+                System.out.println("Pilih opsi");
+                opsi = sc.nextInt();
+                if (opsi == 1) {
+                    int i = 1;
+                    while (true) {
+                        System.out.print("Kode Barang : ");
+                        String kodeBaru = sc.next();
+                        sc.nextLine();
+                        barang[4+i][0] = kodeBaru;
+                        System.out.print("Nama Barang : ");
+                        String namaBaru = sc.nextLine();
+                        barang[4+i][1] = namaBaru;
+                        System.out.print("Lokasi : ");
+                        String lokasiBaru = sc.nextLine();
+                        barang[4+i][2] = lokasiBaru;
+                        System.out.print("Jumlah : ");
+                        int jumlahBaru = sc.nextInt();
+                        jumlah[4+i][0] = jumlahBaru;
+                        sc.nextLine();
 
-                    i++;
+                        i++;
 
-                    System.out.print("Input barang masuk lagi? (y/n): ");
-                    next = sc.nextLine();
+                        System.out.print("Input barang baru lagi? (y/n): ");
+                        next = sc.nextLine();
 
-                    if (next.equalsIgnoreCase("n")) {
-                        break;
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }                    
+                } else if (opsi == 2) {
+                    int i = 1;
+                    while (true) {
+                        System.out.print("Tanggal : ");
+                        String tanggalMasuk = sc.next();
+                        System.out.print("Kode Barang : ");
+                        String kodeBarangMasuk = sc.next();
+                        System.out.print("Nama Barang : ");
+                        sc.nextLine();
+                        String namaBarangMasuk = sc.nextLine();
+                        String lokasiMasuk = "";  // Menyesuaikan nilai pada array data barang
+                        int indexBarangMasuk = -1;
+                        for (i = 1; i < barang.length; i++) {
+                            if (barang[i][0].equals(kodeBarangMasuk)) {
+                                lokasiMasuk = barang[i][2];
+                                indexBarangMasuk = i;
+                                break;
+                            }
+                        }
+                        if (indexBarangMasuk != -1) {
+                            System.out.print("Jumlah Masuk : ");
+                            int jumlahMasuk = sc.nextInt();
+                            jumlah[indexBarangMasuk][0] += jumlahMasuk;
+                            sc.nextLine();
+
+                            System.out.println("Lokasi di " + lokasiMasuk);
+                        } else {
+                            System.out.println("Barang tidak ditemukan.");
+                        }
+
+                        i++;
+
+                        System.out.print("Input barang masuk lagi? (y/n): ");
+                        next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
                     }
                 }
-            
  // barangkeluar
             } else if (lakukan == 3) {
                 int i = 1;
@@ -111,7 +153,7 @@ public class Gudang {
                     String tanggalKeluar = sc.next();
                     System.out.print("Kode Barang : ");
                     String kodeBarangKeluar = sc.next();
-                    System.out.print("Nama Barang  : ");
+                    System.out.print("Nama Barang : ");
                     sc.nextLine();
                     String namaBarangKeluar = sc.nextLine();
                     String lokasiKeluar = "";  // Menyesuaikan nilai pada array data barang
@@ -121,7 +163,6 @@ public class Gudang {
                             lokasiKeluar = barang[i][2];
                             indexBarangKeluar = i;
                             break;
-                            
                         }
                     }
                     if (indexBarangKeluar != -1) {
@@ -129,7 +170,6 @@ public class Gudang {
                         int jumlahKeluar = sc.nextInt();
                         jumlah[indexBarangKeluar][0] -= jumlahKeluar;
                         sc.nextLine();
-
 
                         System.out.println("Lokasi di " + lokasiKeluar);
                     } else {
@@ -144,9 +184,8 @@ public class Gudang {
                     if (next.equalsIgnoreCase("n")) {
                         break;
                     }
-
-            } 
-        }
+                } 
+            }
 
             System.out.print("Lanjut ke menu lain? (y/n): ");
             next = sc.next();
